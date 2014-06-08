b243c34987a439bdcffb343c3dbe843fdcd2b368
Rose Ames <rose@happyspork.com>
2014-06-07 20:07:49 -0400

    add skeleton interpreter

--- repl.asm
+++ repl.asm
@@ -1,19 +1,3 @@
-section .data
-
-prompt1 db ": ", 0
-len1 equ $ - prompt1
-prompt2 db ", ", 0
-len2 equ $ - prompt2
-
-memory times 10000 db 0
-
-buffer times 1000 db 0
-buflen equ $ - buffer
-
-testprog times 72 db "+" ; hopefully H
-db ".", 0
-
-
 section .text
 
 global _start
@@ -42,29 +26,101 @@ _start:
   mov rsi, buffer
   mov rdx, buflen
   syscall
+; number of bytes read is in rax
   
-; print buffer
-  mov rax, 1
-  mov rdi, 1
-  mov rsi, buffer
-  mov rdx, 10
-  syscall
 
 ; while nest_level > 0
 ;   print prompt2
 ;   append input to buffer
 ; execute buffer
+  mov rbx, testprog2
+  jmp execute
 
+; exit(0)
 mov rax, 60
-mov rdi, 0
+mov rdi, 0 
 syscall
 
 execute:
+; instruction ptr is in rbx
 ; case +,-,<,>,[,],., EOF(?) maybe use ! instead
 
+case_add:
+  cmp byte [rbx], '+'
+  jnz case_sub
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, plusok
+  mov rdx, poklen
+  syscall
 
-; helpers :)
+  jmp case_other
 
+case_sub:
+  cmp byte [rbx], '-'
+  jnz case_print
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, minusok
+  mov rdx, moklen
+  syscall
+
+  jmp case_other
+
+case_print:
+  cmp byte [rbx], '.'
+  jnz case_done
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, printok
+  mov rdx, proklen
+  syscall
+
+  jmp case_other
+
+case_done:
+  cmp byte [rbx], '!'
+  jnz case_other
+  mov rax, 60
+  mov rdi, 42 ; return 42 to show we exited intentionally
+  syscall
+
+case_other:
+  inc rbx
+  jmp execute
+
+
+
+section .data
+
+prompt1 db ": ", 0
+len1 equ $ - prompt1
+prompt2 db ", ", 0
+len2 equ $ - prompt2
+
+printok db "-ok", 0
+proklen equ $ - printok
+
+minusok db "-ok", 0
+moklen equ $ - minusok
+
+plusok db "+ok", 0
+poklen equ $ - plusok
+
+otherok db "other ok", 0
+ooklen equ $ - otherok
+
+memory times 10000 db 0
+
+buffer times 1000 db 0
+buflen equ $ - buffer
+
+testprog times 72 db "+" ; hopefully H
+db ".!", 0
+testlen equ $ - testprog
+
+testprog2 db " +,,- ?!++", 0
+testlen2 equ $ - testprog2
 
 
 
